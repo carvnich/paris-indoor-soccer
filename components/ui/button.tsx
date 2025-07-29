a72da07defda97c0a3f625d998b8095e14f5309a
Nicholas Carvalho <nicholasjacarvalho@gmail.com>
2025-07-29 16:05:26 -0400

    add navbar

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -35,24 +35,11 @@ const buttonVariants = cva(
     },
 );
 
-function Button({
-                    className,
-                    variant,
-                    size,
-                    asChild = false,
-                    ...props
-                }: React.ComponentProps<"button"> &
-    VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-}) {
+function Button({className, variant, size, asChild = false, ...props}: React.ComponentProps<"button"> & VariantProps<typeof buttonVariants> & { asChild?: boolean }) {
     const Comp = asChild ? Slot : "button";
 
     return (
-        <Comp
-            data-slot="button"
-            className={cn(buttonVariants({variant, size, className}))}
-            {...props}
-        />
+        <Comp data-slot="button" className={cn(buttonVariants({variant, size, className}))} {...props} />
     );
 }
 
